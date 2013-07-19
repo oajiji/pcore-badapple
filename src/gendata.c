445c3bc5fcf4f5c233b6c72364e2dcd35da6dca9
korepwx <public@korepwx.com>
2013-07-19 14:16:35 +0800

    gendata finished; add playdat for test.

--- src/gendata.c
+++ src/gendata.c
@@ -5,27 +5,73 @@
 #include <libswscale/swscale.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <lz4.h>
+
+#define DST_WIDTH  640
+#define DST_HEIGHT 480
+#define FRAME_RATE 30
+#define MOVIE_SOURCE "/home/pwx/projects/c++/pcore-badapple/video/badapple.mkv"
+#define DATA_PATH    "/home/pwx/projects/c++/pcore-badapple/video/badapple.dat"
+
+static FILE* outfile;
+static int   grpframe, grpcount;
+static uint8_t *picdata, *compressdata;
+static size_t  compress_size, origin_size;
+
+static void OpenDatafile(const char* path) {
+#define PIC_LINE_SIZE  (DST_WIDTH / 2)
+#define PIC_FRAME_SIZE (DST_WIDTH * DST_HEIGHT / 2)
+#define PIC_GROUP_SIZE (FRAME_RATE * PIC_FRAME_SIZE)
+  outfile = fopen(path, "wb");
+  if (outfile == NULL)
+    exit(-1);
+  picdata = (uint8_t*)malloc(PIC_GROUP_SIZE);
+  compressdata = (uint8_t*)malloc(LZ4_compressBound(PIC_GROUP_SIZE));
+  compress_size = origin_size = 0;
+  grpcount = 0; grpframe = 0;
+}
+
+static void CloseDatafile() {
+  free(picdata);
+  free(compressdata);
+  fclose(outfile);
+}
 
 void SaveFrame(AVFrame *pFrame, int width, int height, int iFrame) {
-  FILE *pFile;
-  char szFilename[32];
-  int y;
+  // convert RGB picture to 16-bit grayscale image data.
+  int x, y, k;
+  uint8_t *line = pFrame->data[0];
+  uint8_t *data = picdata + PIC_FRAME_SIZE * (grpframe++);
+  for (y=0; y<height; ++y, line += pFrame->linesize[0], 
+       data += PIC_LINE_SIZE) {
+    // assume data is pure grayscale RGB.
+    for (x=0, k=0; k<width/2; ++k, x+=6) {
+      data[k] = (line[x] & 0xf0) | ((line[x+3] >> 4) & 0x0f);
+    }
+  }
   
-  // Open file
-  sprintf(szFilename, "frame%d.ppm", iFrame);
-  pFile = fopen(szFilename, "wb");
-  if(pFile == NULL)
-    return;
+#if 0
+  char filename[32];
+  sprintf(filename, "frame%d.dat", iFrame);
+  FILE *fp = fopen(filename, "wb");
+  fwrite(data - PIC_FRAME_SIZE, 1, PIC_FRAME_SIZE, fp);
+  fclose(fp);
+#endif
   
-  // Write header
-  fprintf(pFile, "P6\n%d %d\n255\n", width, height);
-  
-  // Write pixel data
-  for(y=0; y<height; y++)
-    fwrite(pFrame->data[0]+y*pFrame->linesize[0], 1, width*3, pFile);
-  
-  // Close file
-  fclose(pFile);
+  // compress the grayscale image data.
+  if (grpframe == FRAME_RATE) {
+    grpframe = 0; ++grpcount;
+    int compressed = LZ4_compress((const char*)picdata, (char*)compressdata, 
+                                  PIC_GROUP_SIZE);
+    if (compressed == 0) 
+      exit(-1);
+    origin_size += PIC_GROUP_SIZE;
+    compress_size += compressed;
+    fwrite(&compressed, 1, sizeof(compressed), outfile);
+    fwrite(compressdata, 1, compressed, outfile);
+    printf("Finish data at %d sec.\n", grpcount);
+    fflush(stdout);
+  }
 }
 
 int main() 
@@ -48,7 +94,7 @@ int main()
   av_register_all();
   
   // Open video file
-  if(avformat_open_input(&pFormatCtx, "badapple-medium.mp4", NULL, NULL)!=0)
+  if(avformat_open_input(&pFormatCtx, MOVIE_SOURCE, NULL, NULL)!=0)
     return -1; // Couldn't open file
     
   // Retrieve stream information
@@ -56,7 +102,7 @@ int main()
     return -1; // Couldn't find stream information
     
   // Dump information about file onto standard error
-  av_dump_format(pFormatCtx, 0, "badapple-medium.mp4", 0);
+  av_dump_format(pFormatCtx, 0, MOVIE_SOURCE, 0);
   
   // Find the first video stream
   videoStream=-1;
@@ -101,8 +147,8 @@ int main()
         pCodecCtx->width,
         pCodecCtx->height,
         pCodecCtx->pix_fmt,
-        pCodecCtx->width,
-        pCodecCtx->height,
+        DST_WIDTH,
+        DST_HEIGHT,
         PIX_FMT_RGB24,
         SWS_BILINEAR,
         NULL,
@@ -114,10 +160,11 @@ int main()
   // Note that pFrameRGB is an AVFrame, but AVFrame is a superset
   // of AVPicture
   avpicture_fill((AVPicture *)pFrameRGB, buffer, PIX_FMT_RGB24, 
-                 pCodecCtx->width, pCodecCtx->height);
+                 DST_WIDTH, DST_HEIGHT);
   
   // Read frames and save first five frames to disk
   i=0;
+  OpenDatafile(DATA_PATH);
   while(av_read_frame(pFormatCtx, &packet)>=0) {
     // Is this a packet from the video stream?
     if(packet.stream_index==videoStream) {
@@ -140,10 +187,8 @@ int main()
         );
 
         // Save the frame to disk
-        if(i >= 200 && i <= 205)
-          SaveFrame(pFrameRGB, pCodecCtx->width, pCodecCtx->height, i);
-        
-        if (++i > 205)
+        SaveFrame(pFrameRGB, DST_WIDTH, DST_HEIGHT, i++);
+        if (i >= 300)
           break;
       }
     }
@@ -151,6 +196,12 @@ int main()
     // Free the packet that was allocated by av_read_frame
     av_free_packet(&packet);
   }
+  if (grpframe != 0) {
+    printf("Finished job unclean with %d frames left.\n", grpframe);
+  }
+  printf("Compress rate %f%%.\n", 
+         (double)compress_size * 100.0 / (double)origin_size);
+  CloseDatafile();
   
   // Free the RGB image
   av_free(buffer);
---
+++ src/playdat.cc
@@ -0,0 +1,81 @@
+#define DST_WIDTH  640
+#define DST_HEIGHT 480
+#define FRAME_RATE 30
+#define PIC_LINE_SIZE  (DST_WIDTH / 2)
+#define PIC_FRAME_SIZE (DST_WIDTH * DST_HEIGHT / 2)
+#define PIC_GROUP_SIZE (FRAME_RATE * PIC_FRAME_SIZE)
+#define DATA_PATH    "/home/pwx/projects/c++/pcore-badapple/video/badapple.dat" 
+
+#include <lz4.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <stdint.h>
+#include <string.h>
+#include <opencv2/opencv.hpp>
+
+cv::Mat image(DST_HEIGHT, DST_WIDTH, CV_8UC3);
+
+static void showGroup(uint8_t* group)
+{
+  int i, x, y;
+  for (i=0; i<30; ++i, group += PIC_FRAME_SIZE) {
+    uint8_t *line = group;
+    
+    for (y=0; y<DST_HEIGHT; ++y, line += PIC_LINE_SIZE) {
+      for (x=0; x<DST_WIDTH/2; ++x) {
+        uint8_t a1, a2;
+        a1 = (line[x] & 0xf0);
+        a2 = (line[x] & 0x0f) << 4;
+        image.at<cv::Vec3b>(y, 2*x) = cv::Vec3b(a1, a1, a1);
+        image.at<cv::Vec3b>(y, 2*x+1) = cv::Vec3b(a2, a2, a2);
+      }
+    }
+    
+    cv::imshow("main", image);
+    cv::waitKey(33);
+  }
+}
+
+int main() 
+{
+  // Initialize opencv.
+  cv::namedWindow("main", CV_WINDOW_AUTOSIZE);
+  
+  // Load all data into memory.
+  FILE *fp = fopen(DATA_PATH, "rb");
+  fseek(fp, 0, SEEK_END);
+  long fsize = ftell(fp);
+  fseek(fp, 0, SEEK_SET);
+  
+  char *data = (char*)malloc(fsize);
+  char *pdata = (char*)malloc(PIC_GROUP_SIZE);
+  if (fread(data, 1, fsize, fp) == 0) {
+    printf("Cannot read data.\n");
+    return -1;
+  }
+  fclose(fp);
+  
+  // Play the frames.
+  char* p = data;
+  int group = 0;
+  
+  for (;;) {
+    // get next group compressed size.
+    int compress_size = *(int*)p;
+    p += sizeof(int);
+    if (compress_size == 0) {
+      break;
+    }
+    // decompress this group.
+    int size = LZ4_decompress_safe(p, pdata, 
+                                   compress_size, PIC_GROUP_SIZE);
+    assert (size == PIC_GROUP_SIZE);
+    p += compress_size; ++group;
+    // Set the ready flag.
+    showGroup((uint8_t*)pdata);
+    printf("[badapple] Decoded %d sec.\n", group);
+  }
+  
+  return 0;
+}
